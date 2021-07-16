4004cd31b056840e51e9f2c33d4167e9dc7bcf28
Jeyson <jeyson@192.168.1.5>
2021-07-15 21:46:02 -0600

    Finish Section 13 - understanding observables

--- Section_13/obs-01-start/src/app/app.component.ts
+++ Section_13/obs-01-start/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './services/user-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class AppComponent implements OnInit, OnDestroy {
+  constructor(private userService: UserService) {}
+  isActivated = false;
+  activatedSubscription: Subscription;
+  ngOnInit() {
+  this.activatedSubscription = this.userService.isActive.subscribe((activationStatus)=>{
+  this.isActivated = activationStatus;
+  })
+  }
+  ngOnDestroy(): void{
+    this.activatedSubscription.unsubscribe();
+  }
 }
--- Section_13/obs-01-start/src/app/home/home.component.ts
+++ Section_13/obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  private firstSubscription : Subscription;
   constructor() { }
 
   ngOnInit() {
+  // this.firstSubscription = interval(1000).subscribe( count => {
+  //   console.log("count", count);
+  // })
+
+  //Building my own observable.
+  let counter = 0;
+  const customObservable = new Observable((observer) => {
+  setInterval(() => {
+    observer.next(counter);
+    if(counter === 2 ){
+      observer.complete();
+    }
+    if(counter > 3){
+      observer.error(new Error(`The dollar value passed from -> ${Math.random()} to -> 0` ))
+    }
+    return counter ++;
+  },1000);
+  });
+
+  customObservable.pipe(map((data) => console.log('data', data)));
+
+  this.firstSubscription = customObservable.pipe(map((data:number) => console.log(`Round: ${data+1}`))).subscribe((data: any) => {
+  }, error => {
+  alert(error.message);
+  },() => {
+    alert('This is the clean up function!');
+  });
   }
+
+
+
+
+  ngOnDestroy(){
+    this.firstSubscription.unsubscribe();
+  }
+
 
 }
